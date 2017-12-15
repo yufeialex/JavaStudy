ed5e23fca5815f833b51d4ba85517bdab49958be
yufeialex <yufeialex@163.com>
2017-12-15 17:42:41 +0800

    future模式的实现

--- language/src/main/java/com/yufei/languagebasic/concurrent/Multi_004/design014/Main.java
+++ language/src/main/java/com/yufei/languagebasic/concurrent/Multi_004/design014/Main.java
@@ -5,12 +5,14 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		
 		FutureClient fc = new FutureClient();
-		Data data = fc.request("请求参数");
+		Future future = fc.request("图谱名称");
 		System.out.println("请求发送成功!");
-		System.out.println("做其他的事情...");
-		
+		System.out.println("去查询并处理图谱数据了...");
+		System.out.println("主线程开始解析图书数据...");
 		Thread.sleep(6000);
-		String result = data.useData();
-		System.out.println(result);
+		System.out.println("主线程解析完了图书数据...");
+		String result = future.get();
+		System.out.println("主线程从异步线程拿回了图谱数据：" + result);
+		System.out.println("主线程开始综合处理");
 	}
 }
