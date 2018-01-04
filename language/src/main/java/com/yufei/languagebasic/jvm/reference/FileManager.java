047bac80bcc3afe5f6412b1fccb3028d39ca6dd2
yufeialex <yufeialex@163.com>
2018-01-04 11:32:23 +0800

    JVM引用

---
+++ language/src/main/java/com/yufei/languagebasic/jvm/reference/FileManager.java
@@ -0,0 +1,38 @@
+package com.yufei.languagebasic.jvm.reference;
+
+import java.io.File;
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by XinYufei on 2018/1/3.
+ */
+public class FileManager {
+    private Map<String, SoftReference<File>> imageCache = new HashMap<String, SoftReference<File>>();
+
+    //保存File的软引用到HashMap
+    public void saveFileToCache(String path) {
+        // 强引用的File对象
+        File File = new File(path);
+        // 软引用的File对象
+        SoftReference<File> softFile = new SoftReference<File>(File);
+        // 添加该对象到Map中使其缓存
+        imageCache.put(path, softFile);
+        // 使用完后手动将位图对象置null
+        File = null;
+    }
+
+    public File getFileByPath(String path) {
+
+        // 从缓存中取软引用的File对象
+        SoftReference<File> softFile = imageCache.get(path);
+        // 判断是否存在软引用
+        if (softFile == null) {
+            return null;
+        }
+        // 取出File对象，如果由于内存不足File被回收，将取得空
+        File File = softFile.get();
+        return File;
+    }
+}
---
+++ language/src/main/java/com/yufei/languagebasic/jvm/reference/WeakRefTest.java
@@ -0,0 +1,54 @@
+package com.yufei.languagebasic.jvm.reference;
+
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by XinYufei on 2018/1/3.
+ */
+public class WeakRefTest {
+    private static ReferenceQueue<MyObject> weakQueue = new ReferenceQueue<>();
+
+    public static class MyObject {
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("MyObject's finalize called");
+        }
+
+        @Override
+        public String toString() {
+            return "I am MyObject";
+        }
+    }
+
+    public static class CheckRefQueue implements Runnable {
+        Reference<MyObject> obj = null;
+
+        @Override
+        public void run() {
+            try {
+                obj = (Reference<MyObject>) weakQueue.remove();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (obj != null) {
+                System.out.println("删除的弱引用为：" + obj + "  but获取弱引用的对象obj.get()=" + obj.get());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyObject object = new MyObject();
+        Reference<MyObject> weakRef = new WeakReference<>(object, weakQueue);
+        System.out.println("创建的弱引用为：" + weakRef);
+        new Thread(new CheckRefQueue()).start();
+
+        object = null;
+        System.out.println("Before GC: Weak Get= " + weakRef.get());
+        System.gc();
+        System.out.println("After GC: Weak Get= " + weakRef.get());
+    }
+}
