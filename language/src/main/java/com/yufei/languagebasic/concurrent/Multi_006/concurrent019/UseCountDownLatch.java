105da0bb05d95338aae4f713f2348766a3c88355
yufeialex <yufeialex@163.com>
2018-01-08 17:32:23 +0800

    countdownLatch

--- language/src/main/java/com/yufei/languagebasic/concurrent/Multi_006/concurrent019/UseCountDownLatch.java
+++ language/src/main/java/com/yufei/languagebasic/concurrent/Multi_006/concurrent019/UseCountDownLatch.java
@@ -4,43 +4,37 @@ import java.util.concurrent.CountDownLatch;
 
 public class UseCountDownLatch {
 
-	public static void main(String[] args) {
-		
-		final CountDownLatch countDown = new CountDownLatch(2);
-		
-		Thread t1 = new Thread(() -> {
-            try {
-                System.out.println("进入线程t1" + "等待其他线程处理完成...");
-                countDown.await();
-                System.out.println("t1线程继续执行...");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        },"t1");
-		
-		Thread t2 = new Thread(() -> {
-            try {
-                System.out.println("t2线程进行初始化操作...");
-                Thread.sleep(3000);
-                System.out.println("t2线程初始化完毕，通知t1线程继续...");
-                countDown.countDown();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-		Thread t3 = new Thread(() -> {
-            try {
-                System.out.println("t3线程进行初始化操作...");
-                Thread.sleep(4000);
-                System.out.println("t3线程初始化完毕，通知t1线程继续...");
-                countDown.countDown();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-		
-		t1.start();
-		t2.start();
-		t3.start();
-	}
+    public static void main(String[] args) {
+
+        int threadCount = 10;
+
+        final CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+
+            new Thread(() -> {
+
+                System.out.println("运动员" + Thread.currentThread().getId() + "开始出发");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("运动员" + Thread.currentThread().getId() + "已到达终点");
+
+                latch.countDown();
+            }).start();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("10个运动员已经执行完毕！开始计算排名");
+    }
+
 }
---
+++ language/src/main/java/com/yufei/languagebasic/concurrent/Multi_006/countdownLatch1/CacheHealthChecker.java
@@ -0,0 +1,27 @@
+package com.yufei.languagebasic.concurrent.Multi_006.countdownLatch1;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by XinYufei on 2018/1/8.
+ */
+public class CacheHealthChecker extends BaseHealthChecker{
+    public CacheHealthChecker (CountDownLatch latch)  {
+        super("Cache Service", latch);
+    }
+
+    @Override
+    public void verifyService()
+    {
+        System.out.println("Checking " + this.getServiceName());
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println(this.getServiceName() + " is UP");
+    }
+}
