dd4f4418e5182a16cbb51e69a91905a897b4ff03
yufeialex <yufeialex@163.com>
2018-01-12 16:58:24 +0800

    最大线程数测试

---
+++ language/src/main/java/com/yufei/languagebasic/jvm/TestMaxSupportThreads.java
@@ -0,0 +1,47 @@
+package com.yufei.languagebasic.jvm;
+
+/**
+ * Created by XinYufei on 2018/1/11.
+ */
+public class TestMaxSupportThreads {
+    private static final class TestThread extends Thread {
+        private final int number;
+
+        public TestThread(int number) {
+            this.number = number;
+        }
+
+        public TestThread(int number, int stackSize) {
+            super(null, null, "Thread-Hi-" + number, stackSize);
+            this.number = number;
+        }
+
+        @Override
+        public void run() {
+            if (number % 100 == 0) {
+                System.out.println("Thread " + number + " started.");
+            }
+            try {
+                Thread.sleep(Long.MAX_VALUE);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        try {
+            for (i = 0; i < 200; i++) {
+                Thread t = new TestThread(i, 2 << 19);//512K
+                t.start();
+            }
+        } catch (Throwable e) {
+            System.out.println("Error occured when creating thread " + i);
+            e.printStackTrace();
+        }
+    }
+}
