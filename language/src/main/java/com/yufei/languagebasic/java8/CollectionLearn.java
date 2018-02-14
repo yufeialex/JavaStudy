778a4592db139cc98338b60f3c72e1f6943b35c3
yufeialex <yufeialex@163.com>
2018-02-14 14:08:55 +0800

    java8集合操作

---
+++ language/src/main/java/com/yufei/languagebasic/java8/CollectionLearn.java
@@ -0,0 +1,19 @@
+package com.yufei.languagebasic.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CollectionLearn {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        System.out.println(integers);
+        List<Integer> collect = integers.stream()
+                .map(a -> a.equals(1) ? 8 : a)
+                .collect(Collectors.toList());
+        System.out.println(collect);
+    }
+}
